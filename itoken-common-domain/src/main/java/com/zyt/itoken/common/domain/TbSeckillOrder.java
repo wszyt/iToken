19ac72a09699d2a9582fad9bf57ef432ffd1bf7f
wszyt <173982112@qq.com>
2019-07-11 22:40:10 +0800

    commit ms n

--- itoken-common-domain/src/main/java/com/zyt/itoken/common/domain/TbSeckillOrder.java
+++ itoken-common-domain/src/main/java/com/zyt/itoken/common/domain/TbSeckillOrder.java
@@ -7,72 +7,100 @@ import javax.persistence.*;
 @Table(name = "tb_seckill_order")
 public class TbSeckillOrder implements Serializable {
     private static final long serialVersionUID = 4407269770636526206L;
-    @Id
+    /**
+     * 秒杀商品id
+     */
     @Column(name = "seckill_id")
     private Long seckillId;
 
+    /**
+     * 用户手机号
+     */
     @Column(name = "user_phone")
     private Long userPhone;
 
+    /**
+     * 状态标示:-1:无效 0:成功 1:已付款 2:已发货
+     */
     private Byte state;
 
+    /**
+     * 创建时间
+     */
     @Column(name = "create_time")
     private Date createTime;
 
     /**
-     * @return seckill_id
+     * 获取秒杀商品id
+     *
+     * @return seckill_id - 秒杀商品id
      */
     public Long getSeckillId() {
         return seckillId;
     }
 
     /**
-     * @param seckillId
+     * 设置秒杀商品id
+     *
+     * @param seckillId 秒杀商品id
      */
     public void setSeckillId(Long seckillId) {
         this.seckillId = seckillId;
     }
 
     /**
-     * @return user_phone
+     * 获取用户手机号
+     *
+     * @return user_phone - 用户手机号
      */
     public Long getUserPhone() {
         return userPhone;
     }
 
     /**
-     * @param userPhone
+     * 设置用户手机号
+     *
+     * @param userPhone 用户手机号
      */
     public void setUserPhone(Long userPhone) {
         this.userPhone = userPhone;
     }
 
     /**
-     * @return state
+     * 获取状态标示:-1:无效 0:成功 1:已付款 2:已发货
+     *
+     * @return state - 状态标示:-1:无效 0:成功 1:已付款 2:已发货
      */
     public Byte getState() {
         return state;
     }
 
     /**
-     * @param state
+     * 设置状态标示:-1:无效 0:成功 1:已付款 2:已发货
+     *
+     * @param state 状态标示:-1:无效 0:成功 1:已付款 2:已发货
      */
     public void setState(Byte state) {
         this.state = state;
     }
 
     /**
-     * @return create_time
+     * 获取创建时间
+     *
+     * @return create_time - 创建时间
      */
     public Date getCreateTime() {
         return createTime;
     }
 
     /**
-     * @param createTime
+     * 设置创建时间
+     *
+     * @param createTime 创建时间
      */
     public void setCreateTime(Date createTime) {
         this.createTime = createTime;
     }
+
 
 }
--- itoken-service-seckill/src/main/java/com/zyt/itoken/service/seckill/service/impl/SpikeCommodityServiceImpl.java
+++ itoken-service-seckill/src/main/java/com/zyt/itoken/service/seckill/service/impl/SpikeCommodityServiceImpl.java
@@ -3,6 +3,7 @@ package com.zyt.itoken.service.seckill.service.impl;
 import com.alibaba.fastjson.JSONObject;
 import com.zyt.itoken.service.seckill.common.BaseApiService;
 import com.zyt.itoken.service.seckill.common.BaseResponse;
+import com.zyt.itoken.service.seckill.common.RedisUtil;
 import com.zyt.itoken.service.seckill.service.SpikeCommodityService;
 import com.zyt.itoken.common.domain.TbSeckill;
 import com.zyt.itoken.common.domain.TbSeckillOrder;
@@ -11,6 +12,7 @@ import com.zyt.itoken.common.mapper.TbSeckillOrderMapper;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class SpikeCommodityServiceImpl extends BaseApiService<JSONObject> implements SpikeCommodityService {
@@ -20,7 +22,11 @@ public class SpikeCommodityServiceImpl extends BaseApiService<JSONObject> implem
     @Autowired
     private TbSeckillOrderMapper tbSeckillOrderMapper;
 
+    @Autowired
+    RedisUtil redisUtil;
+
     @Override
+    @Transactional
     public BaseResponse<JSONObject> spike(String phone, Long seckillId) {
         // 1.参数验证
         if (StringUtils.isEmpty (phone)) {
@@ -29,11 +35,28 @@ public class SpikeCommodityServiceImpl extends BaseApiService<JSONObject> implem
         if (seckillId == null) {
             return setResultError("商品库存id不能为空");
         }
+
+
+        // 2. 从 redis 中获取对应的秒杀 Token
+
+
+        // 3. 获取到 token 中放入 mq 中实现修改商品的库存
+
+
+
+
+        /**
         // 2.用户频率验证，做限制
+        Boolean aBoolean = redisUtil.setNx (phone, seckillId + "", 10L);
+        if (!aBoolean) {
+            return setResultError ("访问次数过多，请10秒后再次访问");
+        }
+
+        // 100 个商品可以提前生成 100 个 token，谁能抢到 token 谁才能秒杀成功放入 mq 中进行异步减库存（商品抢购）
         // 3.修改数据库对应库存
         TbSeckill entry = tbSeckillMapper.findBySeckillId (seckillId);
         if (entry == null) {
-            setResultError ("商品信息不存在");
+            return setResultError ("商品信息不存在");
         }
 
         int invenroeyDeduction = tbSeckillMapper.invenroeyDeduction (seckillId, entry.getVersion ());
@@ -45,10 +68,11 @@ public class SpikeCommodityServiceImpl extends BaseApiService<JSONObject> implem
         Long number = Long.parseLong (phone);
         tbSeckillOrder.setUserPhone (number);
         tbSeckillOrder.setSeckillId (seckillId);
-        int insert = tbSeckillOrderMapper.insert (tbSeckillOrder);
+        int insert = tbSeckillOrderMapper.insertOrder (tbSeckillOrder);
         if (insert != 1) {
             return setResultError("请稍后重试");
         }
+         **/
 
         return setResultSuccess("抢购成功");
     }
