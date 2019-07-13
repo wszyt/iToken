fdd248bc5c6ddeab4dbd51daad907ac047138266
wszyt <173982112@qq.com>
2019-07-13 15:03:09 +0800

    commit ms nn

--- itoken-service-seckill/src/main/java/com/zyt/itoken/service/seckill/SeckillApplication.java
+++ itoken-service-seckill/src/main/java/com/zyt/itoken/service/seckill/SeckillApplication.java
@@ -3,10 +3,12 @@ package com.zyt.itoken.service.seckill;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableAsync;
 import tk.mybatis.spring.annotation.MapperScan;
 
 
 // 秒杀服务，因为并发较高需要另开一个数据库
+@EnableAsync
 @EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "com.zyt.itoken")
 @MapperScan(basePackages = {"com.zyt.itoken.common.mapper"})
